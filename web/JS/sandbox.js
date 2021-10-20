3b3de0a8df8a33c0c8c74442045b74bc6f5da4a1
Masandza <82656809+Ma-Sandza@users.noreply.github.com>
2021-10-20 07:56:06 +0200

    Added an update page

--- web/JS/sandbox.js
+++ web/JS/sandbox.js
@@ -1,9 +1,8 @@
 function dropdown(){
     var x = document.getElementById("ul");
     if ( x.style.display === "none"){
-        x.style.display = "flex"
+        x.style.display = "flex";
     } else {
-        x.style.display = "none"
+        x.style.display = "none";
     }
-   
 }
